de5dcca7a9207b526be1723b378cbf49b54abed0
Manfred <manfredcb@gmail.com>
2023-11-21 00:22:39 -0300

    add exercise 5

--- TP6/exercise_5.h
+++ TP6/exercise_5.h
@@ -22,12 +22,37 @@
 
 */
 
-void exercise_5() {
+int exercise_5() {
 	printf("\t\t***************************\n");
 	printf("\t\t**      EJERCICIO 5      **\n");
 	printf("\t\t***************************\n\n");
 	
+	FILE *archivo = fopen("productos.txt", "r");
+	
+	if(archivo == NULL){
+		printf("No se pudo abrir el archivo productos.txt");
+		return 1;
+	}
+	
+	int codProducto, i = 0;
+	float peso;
+	char vec[100][20] = {""};
+	char producto[20];
+	char kg[2];
+	
+	while(fscanf(archivo, "%d\t%s\t%f %s", &codProducto, producto, &peso, kg) == 4){
+		strcpy(vec[codProducto], producto);
+		i++;
+	}
+	
+	i = 0;
+	
+	for(i = 0; i< 100; i++){
+		if(strcmp(vec[i], "") != 0)
+			printf("vec[%d] = %s\n", i, vec[i]);
+	}
 		
+	fclose(archivo);
 }
 
 
